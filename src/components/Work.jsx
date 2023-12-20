f4b01894b4357d70e332c162976bcb42836f0804
ayoub <ayoub-ok147@hotmail.fr>
2023-12-20 16:07:49 +0100

    mod

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Work = () => {
   return (
-    <div className='work'>
-        <div className='container'>
-            .t
+    <div className="work">
+      <div className="container">
+        <div className="title">
+          <h1 id="work">Work</h1>
+          <p>Check out my some of my recent work</p>
         </div>
-    </div>
-  )
-}
 
-export default Work
+        <div className="projects">
+            <article className="project1">
+                <div>
+                    <span></span>
+                </div>
+            </article>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
