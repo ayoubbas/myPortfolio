d9e3e77d6ddfdfc4533a16c3f30a8fe8a7dd5781
ayoub <ayoub-ok147@hotmail.fr>
2023-12-20 15:52:47 +0100

    add imgs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
-import './MediaQueries.css';
-import Home from './components/Home';
-import NavBar from './components/NavBar';
+import "./App.css";
+import "./MediaQueries.css";
+import About from "./components/About";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
 
 function App() {
   return (
     <div>
-      <NavBar/>
+      <NavBar />
       <Home />
+      <About />
+      <Skills />
+      <Work />
     </div>
   );
 }
